8ce824fbe54a9552d72f4146b74c5462b23435a3
Insha <ik3166016@gmail.com>
2025-06-28 15:18:36 +0500

    fixed some issues in install_dependencies.sh

--- deploy/scripts/install_dependencies.sh
+++ deploy/scripts/install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Update and install Docker
 sudo apt-get update -y
 sudo apt-get install docker.io -y
@@ -12,12 +14,12 @@ sudo systemctl enable docker
 sudo apt-get install unzip curl -y
 
 # Download and install AWS CLI v2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -o awscliv2.zip
-sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip"
+unzip -o /home/ubuntu/awscliv2.zip -d /home/ubuntu/
+sudo /home/ubuntu/aws/install
 
 # Add ubuntu user to the docker group (so you can run docker without sudo)
 sudo usermod -aG docker ubuntu
 
 # Clean up installer files
-rm -rf awscliv2.zip aws
+rm -rf /home/ubuntu/awscliv2.zip /home/ubuntu/aws
